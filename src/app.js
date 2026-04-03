281c14eed2d9efeb52c039dcae2f201e8b7f7848
akkarapon <qwasdzx225@gmail.com>
2026-04-03 12:19:10 +0700

    my project

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const { title } = require('process');
 
 const app = express();
 app.set("views", path.join(__dirname, "views"))
@@ -18,8 +19,8 @@ app.get("/", (req, res) => {
         { id: 9, img: "/image/git.png", skill: 50 },
     ];
     const workData = [
-        { id: 1,img: "/image/works/1.png",content: "ระบบ"},
-        { id: 2,img: "/image/works/1.png",content: "ระบบ"},
+        { id: 1,img: "/image/works/ระบบสั่งกาแฟ.png",title: "ระบบสั่งกาแฟ และรับออเดอร์แบบเรียลไทม์", content: "ระบบจัดการและรับออเดอร์แบบเรียลไทม์ พัฒนาฝั่ง Back-end ด้วย Node.js เป็นหลัก พร้อมประยุกต์ใช้เทคโนโลยี Socket.io เพื่อจัดการการรับ-ส่งข้อมูลแบบสองทาง ช่วยให้ระบบสามารถอัปเดตสถานะคำสั่งซื้อได้ทันที ตอบสนองรวดเร็วโดยที่ผู้ใช้งานไม่ต้องรีเฟรชหน้าจอ"},
+        { id: 2,img: "/image/works/line-chat.png",title: "ไลน์แชทบอท AI (โปรเจคจบมหาลัย)", content: "ระบบแชทบอทอัจฉริยะบนแพลตฟอร์ม LINE พัฒนา Webhook ด้วย Node.js เพื่อเชื่อมต่อกับโมเดลภาษา (DeepSeek API) พร้อมบูรณาการฐานข้อมูล Qdrant (Vector Database) ในการช่วยให้ AI สามารถค้นหาข้อมูลเชิงความหมายและตอบคำถามได้อย่างแม่นยำ"},
     ];
   
   // ส่งตัวแปร skillsData ไปที่ไฟล์ index.ejs ในชื่อ "skills"
